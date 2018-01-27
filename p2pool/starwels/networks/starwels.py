345423802eca4d68315f52998024397969e07322
SHI <deepdonfrenki@gmail.com>
2018-01-27 23:16:51 +0300

    Abbreviations

--- p2pool/starwels/networks/starwels.py
+++ p2pool/starwels/networks/starwels.py
@@ -18,7 +18,7 @@ RPC_CHECK = defer.inlineCallbacks(lambda starwelsd: defer.returnValue(
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
 POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
-SYMBOL = 'AI'
+SYMBOL = 'MAI'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Starwels') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Starwels/') if platform.system() == 'Darwin' else os.path.expanduser('~/.starwels'), 'starwels.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'https://localhost/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'https://localhost/address/'
